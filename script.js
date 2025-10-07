3b0d54c3ea98668ce5811937d6f44c0b8630777c
Warren Nelson <nelsonwarren155@gmail.com>
2025-10-07 19:16:25 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+/*
+
+Voici mon tout premier projet.
+
+Une simple page web affichant le fameux "Hello, World" aussi bien
+en console que dans le rendu visuel de la page.
+
+Par ailleurs, elle possède aussi un mode sombre et clair, activable via un bouton
+contenant ce texte : "Changez le thème !"
+
+Malgré le fait que je sois un novice dans le monde de la programmation, j'ai
+néanmoins essayée de styliser ma page web, certe à ma façon mais j'ai au moins
+
+le mérite de l'avoir fait !
+
+*/
